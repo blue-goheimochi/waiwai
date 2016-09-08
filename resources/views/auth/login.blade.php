4d69c5c70acec4f160b0fffc545f1270762f5a67
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-09-08 19:20:47 +0900

    update view for login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,37 @@
-<h1>ログイン</h1>
-<form action="/login" method="POST">
-  <div>
-    <label for="email">メールアドレス</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}">
-    @if ($errors->has('email'))
-    <div class="errors"><p>{{ $errors->first('email') }}</p></div>
-    @endif
-  </div>
-
-  <div>
-    <label for="password">パスワード</label>
-    <input id="password" type="password" name="password">
-    @if ($errors->has('password'))
-    <div class="errors"><p>{{ $errors->first('password') }}</p></div>
-    @endif
-  </div>
-
-  <div>
-    <label for="remember">
-      <input id="remember" type="checkbox" name="remember"{!! old('remember') ? ' checked="checked"' : '' !!} > ログインしたままにする
-    </label>
-
-    <button type="submit" class="pure-button">ログイン</button>
-  </div>
-
-  {!! csrf_field() !!}
-</div>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>ログイン</title>
+    <link href="/css/app.css" rel="stylesheet">
+  </head>
+  <body id="login">
+    <div class="container">
+      <form action="/login" method="POST" class="form-signin">
+        <h2 class="form-signin-heading">ログイン</h2>
+        @if (count($errors) > 0)
+        <div class="alert alert-danger" role="alert">
+          @foreach ($errors->all() as $error)
+          {{ $error }}
+          @endforeach
+        </div>
+        @endif
+        <label for="email" class="sr-only">メールアドレス</label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="メールアドレス" required autofocus>
+        <label for="password" class="sr-only">パスワード</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="パスワード" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox"id="remember" value="remember-me"{!! old('remember') ? ' checked="checked"' : '' !!}> 次回から自動的にログインする
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
+        {!! csrf_field() !!}
+      </form>
+    </div>
+  </body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,38 @@
-<h1>ユーザー登録</h1>
-<form action="/register" method="POST">
-  <div>
-    <label for="name">ユーザー名</label>
-    <input id="name" type="text" name="name" value="{{ old('name') }}">
-    @if ($errors->has('name'))
-    <div class="errors"><p>{{ $errors->first('name') }}</p></div>
-    @endif
-  </div>
-
-  <div>
-    <label for="email">メールアドレス</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}">
-    @if ($errors->has('email'))
-    <div class="errors"><p>{{ $errors->first('email') }}</p></div>
-    @endif
-  </div>
-
-  <div>
-    <label for="password">パスワード</label>
-    <input id="password" type="password" name="password">
-    @if ($errors->has('password'))
-    <div class="errors"><p>{{ $errors->first('password') }}</p></div>
-    @endif
-  </div>
-
-  <div>
-    <label for="password_confirmation">パスワード確認</label>
-    <input id="password_confirmation" type="password" name="password_confirmation">
-    @if ($errors->has('password_confirmation'))
-    <div class="errors"><p>{{ $errors->first('password_confirmation') }}</p></div>
-    @endif
-  </div>
-
-  <div>
-    <button type="submit">登録</button>
-  </div>
-
-  {!! csrf_field() !!}
-</form>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>ユーザー登録</title>
+    <link href="/css/app.css" rel="stylesheet">
+  </head>
+  <body id="register">
+    <div class="container">
+      <form action="/register" method="POST" class="form-signin">
+        <h2 class="form-signin-heading">ユーザー登録</h2>
+        <label for="name" class="sr-only">ユーザーID</label>
+        <input type="text" id="name" name="name" value="{{ old('name') }}" class="form-control" placeholder="ユーザー名" autofocus>
+        @if ($errors->has('name'))
+        <div class="alert alert-danger" role="alert">{{ $errors->first('name') }}</div>
+        @endif
+        <label for="email" class="sr-only">メールアドレス</label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="メールアドレス" autofocus>
+        @if ($errors->has('email'))
+        <div class="alert alert-danger" role="alert">{{ $errors->first('email') }}</div>
+        @endif
+        <label for="password" class="sr-only">パスワード</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="パスワード">
+        <label for="password_confirmation" class="sr-only">パスワード確認</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="パスワード確認">
+        @if ($errors->has('password'))
+        <div class="alert alert-danger" role="alert">{{ $errors->first('password') }}</div>
+        @endif
+        <button class="btn btn-lg btn-primary btn-block" type="submit">登録</button>
+        {!! csrf_field() !!}
+      </form>
+    </div>
+  </body>
+</html>
