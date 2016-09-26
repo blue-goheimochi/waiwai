9be10fe843fee2c977b1ebd61214a6f681bef6c9
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-09-27 02:00:55 +0900

    edit layout for topic detail

--- resources/views/topic/detail.blade.php
+++ resources/views/topic/detail.blade.php
@@ -2,16 +2,29 @@
 
 @section('pageTitle', 'トピック詳細')
 @section('pageDescription', 'トピック詳細ページです')
-@section('pageId', 'add_topic')
+@section('pageId', 'topic_detail')
 
 @section('container')
 <div class="container">
-  <div class="form-signin">
-    <h2 class="form-signin-heading">トピック詳細</h2>
-    <h3>タイトル</h3>
-    <p>{{ $topic->title }}</p>
-    <h3>本文</h3>
-    <p>{!! nl2br(e($topic->body)) !!}</p>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="date">投稿日時: {{ $topic->created_at }}</div>
+      <h1>{{ $topic->title }}</h1>
+      <div class="questioner text-right">質問者：{{ $topic->user->name }}</div>
+    </div>
+    <div class="col-md-8">
+      <p class="body">{!! nl2br(e($topic->body)) !!}</p>
+      <h2>コメント投稿</h2>
+      @if (Auth::guest())
+      <div class="comment_no_login"><p class="text-center">コメントするには<a href="/login">ログイン</a>してください</p></div>
+      @else
+      <div class="comment_btn"><a class="btn btn-outline-secondary btn-block  ">コメントする</a></div>
+      @endif
+    </div>
+    <div class="col-sm-4">
+      <div class="ad" style="width: 300px; margin: 0 auto 5px;"><div style="width: 290px; height: 240px; background: #f0f8ff; border: 5px solid #483d8b;"></div></div>
+      <div class="ad" style="width: 300px; margin: 0 auto 5px;"><div style="width: 290px; height: 240px; background: #ffffe0; border: 5px solid #f0e68c;"></div></div>
+    </div>
   </div>
 </div>
 @endsection
