896af02bfe5ccd92ba78b3648b730fd76fe792d9
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-09-26 12:31:12 +0900

    add topics table

---
+++ database/migrations/2015_01_01_000000_create_topics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title', 500);
+            $table->text('body', 500);
+            $table->integer('status')->unsigned()->default(1);
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('topics');
+    }
+}
