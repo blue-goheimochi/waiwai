93dc8a0d0af17a3fce9ab57c2f9542aaaac39f54
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-09-27 13:15:12 +0900

    add comments table

---
+++ database/migrations/2015_01_01_100000_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('topic_id')->unsigned();
+            $table->integer('parent_comment_id')->unsigned()->nullable();
+            $table->text('body');
+            $table->integer('status')->unsigned()->default(1);
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('topic_id')->references('id')->on('topics');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
