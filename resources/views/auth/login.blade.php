67a24372d699be74a1a870645d32d9009cc001a9
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-11-03 23:33:36 +0900

    edit layout for login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,26 +6,26 @@
 
 @section('container')
 <div class="container">
-  <form action="/login" method="POST" class="form-signin">
-    <h2 class="form-signin-heading">ログイン</h2>
-    @if (count($errors) > 0)
-    <div class="alert alert-danger" role="alert">
-      @foreach ($errors->all() as $error)
-      {{ $error }}
-      @endforeach
-    </div>
-    @endif
-    <label for="email" class="sr-only">メールアドレス</label>
-    <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="メールアドレス" required autofocus>
-    <label for="password" class="sr-only">パスワード</label>
-    <input type="password" id="password" name="password" class="form-control" placeholder="パスワード" required>
-    <div class="checkbox">
-      <label>
-        <input type="checkbox"id="remember" value="remember-me"{!! old('remember') ? ' checked="checked"' : '' !!}> 次回から自動的にログインする
-      </label>
-    </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
-    {!! csrf_field() !!}
-  </form>
+  <div class="login-box">
+    <form action="/login" method="POST" class="form-signin">
+      <h2 class="form-signin-heading">Sample App</h2>
+      @if (count($errors) > 0)
+      <div class="alert alert-danger" role="alert">
+        @foreach ($errors->all() as $error)
+        {{ $error }}<br>
+        @endforeach
+      </div>
+      @endif
+      @if (session('message'))
+      <div class="alert alert-danger" role="alert">{{ session('message') }}</div>
+      @endif
+      <label for="email" class="sr-only">メールアドレス</label>
+      <input type="text" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="メールアドレス" autofocus>
+      <label for="password" class="sr-only">パスワード</label>
+      <input type="password" id="password" name="password" class="form-control" placeholder="パスワード">
+      <button class="btn btn-lg btn-primary btn-block btn-login" type="submit">ログイン</button>
+      {!! csrf_field() !!}
+    </form>
+  </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,27 +6,29 @@
 
 @section('container')
 <div class="container">
-  <form action="/register" method="POST" class="form-signin">
-    <h2 class="form-signin-heading">ユーザー登録</h2>
-    <label for="name" class="sr-only">ユーザーID</label>
-    <input type="text" id="name" name="name" value="{{ old('name') }}" class="form-control" placeholder="ユーザー名" autofocus>
-    @if ($errors->has('name'))
-    <div class="alert alert-danger" role="alert">{{ $errors->first('name') }}</div>
-    @endif
-    <label for="email" class="sr-only">メールアドレス</label>
-    <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="メールアドレス" autofocus>
-    @if ($errors->has('email'))
-    <div class="alert alert-danger" role="alert">{{ $errors->first('email') }}</div>
-    @endif
-    <label for="password" class="sr-only">パスワード</label>
-    <input type="password" id="password" name="password" class="form-control" placeholder="パスワード">
-    <label for="password_confirmation" class="sr-only">パスワード確認</label>
-    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="パスワード確認">
-    @if ($errors->has('password'))
-    <div class="alert alert-danger" role="alert">{{ $errors->first('password') }}</div>
-    @endif
-    <button class="btn btn-lg btn-primary btn-block" type="submit">登録</button>
-    {!! csrf_field() !!}
-  </form>
+  <div class="register-box">
+    <form action="/register" method="POST" class="form-signin">
+      <h2 class="form-signin-heading">ユーザー登録</h2>
+      <label class="input-label" for="name">ユーザー名</label>
+      <input type="text" id="name" name="name" value="{{ old('name') }}" class="form-control" placeholder="sample" autofocus>
+      @if ($errors->has('name'))
+      <div class="alert alert-danger" role="alert">{{ $errors->first('name') }}</div>
+      @endif
+      <label class="input-label" for="email">メールアドレス</label>
+      <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="sample@test.com" autofocus>
+      @if ($errors->has('email'))
+      <div class="alert alert-danger" role="alert">{{ $errors->first('email') }}</div>
+      @endif
+      <label class="input-label" for="password">パスワード</label>
+      <input type="password" id="password" name="password" class="form-control" placeholder="6文字以上で入力">
+      <label class="input-label" for="password_confirmation">パスワード確認</label>
+      <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="上記と同じ内容で入力">
+      @if ($errors->has('password'))
+      <div class="alert alert-danger" role="alert">{{ $errors->first('password') }}</div>
+      @endif
+      <button class="btn btn-lg btn-primary btn-block btn-register" type="submit">登録</button>
+      {!! csrf_field() !!}
+    </form>
+  </div>
 </div>
 @endsection
